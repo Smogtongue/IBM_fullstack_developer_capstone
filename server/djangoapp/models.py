f4349ff5f7e55b00a69e42837d0fc45115b67f7a
Zee <smogtongue@gmail.com>
2025-08-20 12:39:51 -0700

    Lint Python errors, continuing to fix.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -50,13 +50,12 @@ class CarModel(models.Model):
         ('EV', 'Electric'),
     ]
     condition = models.CharField(max_length=10, choices=CAR_CONDITIONS,
-                                default='NEW')
+                                 default='NEW')
 
     color = models.CharField(max_length=30, blank=True)
     mileage = models.IntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True,
-                               blank=True)
+                                blank=True)
 
     def __str__(self):
         return self.name
-
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://elzofchak-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ('https://elzofchak-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+     'proxy.cognitiveclass.ai')
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://elzofchak-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ('https://elzofchak-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+     'proxy.cognitiveclass.ai')
 ]
 
 REST_FRAMEWORK = {
@@ -98,16 +100,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'UserAttributeSimilarityValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'MinimumLengthValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'CommonPasswordValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'NumericPasswordValidator'),
     },
 ]
 
@@ -144,4 +150,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
